d8d47a0e178a03b4c133da22f3de23ea47f8c3ec
NgocHien307 <hiennnhe187362@fpt.edu.vn>
2025-11-02 22:26:22 +0700

    Project Initializr

---
+++ src/main/java/com/group5/dss/DssApplication.java
@@ -0,0 +1,13 @@
+package com.group5.dss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group5/dss/DssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group5.dss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
